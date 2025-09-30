e4943fec51bd888c03b1a457bd2d0b0152bc666f
mattilagan1 <ilagan.matthew67@gmail.com>
2025-09-30 19:51:32 +1000

    Implimented Current User Method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session,
   if: -> { request.format.json? }
+
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split (" ").last if header
+    begin 
+      decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      @current_user = User.find(decoded["user_id"])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+      render json: { errors: "Unauthorized" }, status: :unauthorized
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authorize_request
+
   def create
     user = User.new(
       name: params[:name],
